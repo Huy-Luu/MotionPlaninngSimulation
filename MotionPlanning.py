e3290d6cdc4d412ef6b3bfd54da85e3fbe62856c
Huy Luu <huyluu1820@gmail.com>
2022-09-08 21:51:46 +0700

    [Path planning simulation] Working on Points, UTM and PathGenerator modules

--- MotionPlanning.py
+++ MotionPlanning.py
@@ -1,7 +1,14 @@
 import numpy as np
 from Vehicle import Vehicle
+from UTMmodule import UTMmodule
+from Point import OriginalPoint
 
 print("Init car")
 vehicle = Vehicle(0.0, 0.0, 0.0, 0.0, 30)
-vehicle.plot_car(1.0, 1.0, 1.0)
+#vehicle.plot_car(1.0, 1.0, 1.0)
+op1 = OriginalPoint(10.772580, 106.658847)
+op2 = OriginalPoint(10.773004, 106.659656)
+op3 = OriginalPoint(10.772529, 106.659708)
+#print("Point 1: " + str(op1.getLat()) + " and " + str(op1.getLon()))
+# print(UTMmodule.zone_number_to_central_longitude(2))
 print("Plotted car")
---
+++ Point.py
@@ -0,0 +1,23 @@
+
+class Point(object):
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def getX():
+        return self.x
+
+    def getY():
+        return self.y
+
+class OriginalPoint(object):
+    def __init__(self, lat, lon):
+        self.lat = lat
+        self.lon = lon
+
+    def getLat(self):
+        return self.lat
+
+    def getLon(self):
+        return self.lon
