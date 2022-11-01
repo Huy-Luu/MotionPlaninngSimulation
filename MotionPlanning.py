0d7d67dd6efad80a74526098ec89aaa2ecc53b21
Huy Luu <huyluu1820@gmail.com>
2022-11-01 21:56:15 +0700

    Working simulation with MQTT communication from UI

--- MotionPlanning.py
+++ MotionPlanning.py
@@ -21,21 +21,8 @@ sw = SlidingWindow(10)
 client = MQTTclient("broker.hivemq.com", 1883, "SimulationCart")
 client.init("control/auto")
 
-#Initiate a set of original points
-og_points = []
-#vehicle.plotCar(1.0, 1.0, 1.0)
-#op1 = OriginalPoint(10.772580, 106.658847)
-#op2 = OriginalPoint(10.773004, 106.659656)
-#op3 = OriginalPoint(10.772529, 106.659708)
-# og_points.append(OriginalPoint(10.772580, 106.658847))
-# og_points.append(OriginalPoint(10.773004, 106.659656))
-# og_points.append(OriginalPoint(10.772529, 106.659708))
-# og_points.append(OriginalPoint(10.772640, 106.659920))
 
-# og_points.append(OriginalPoint(10.772972, 106.659762))
-# og_points.append(OriginalPoint(10.772879, 106.660016))
-# og_points.append(OriginalPoint(10.772824, 106.659838))
-# og_points.append(OriginalPoint(10.773053, 106.659897))
+og_points = []
 
 while(client.waypointcame == False):
     pass
@@ -75,11 +62,6 @@ last_idx = len(path_x) -1
 target_idx, _ = scontroller.calcTargetIndex(vehicle, path, 0)
 print(target_idx)
 
-
-
 Simulation.simulate(vehicle, dt, 500, 3, client, scontroller, sw, utm, target_idx, last_idx, path, waypoints, waypoint_indices, offset, yaw, True)
 
 
-#print("Point 1: " + str(op1.getLat()) + " and " + str(op1.getLon()))
-# print(UTMmodule.zone_number_to_central_longitude(2))
-print("Plotted car")
